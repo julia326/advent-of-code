4841f415665ad6e259ef92c6390bab964fdf7047
Julia K <julia326@gmail.com>
2020-12-10 13:54:41 -0500

    day 10

--- day10.py
+++ day10.py
@@ -15,36 +15,18 @@ def puzzle1(jolts):
 def is_path(cur_num, target, jolts):
     if cur_num == target:  # path is valid and reached end
         return 1
-    if cur_num not in jolts:
-        return 0
-    return is_path(cur_num + 1, target, jolts) + \
-        is_path(cur_num + 2, target, jolts) + \
-        is_path(cur_num + 3, target, jolts)
-
-
-def puzzle2(jolts):
-    start = time.time()
-    print(max(jolts))
-    jolts = set(jolts)
-    print(is_path(0, max(jolts), jolts))
-    end = time.time()
-    print(end - start)
-
-
-def is_path_more_efficient(cur_num, target, jolts):
-    if cur_num == target:  # path is valid and reached end
-        return 1
-    if cur_num not in jolts:
+    if cur_num > target:  # invalid path
         return 0
 
-    # remove current element before passing down the tree
-    jolts.discard(cur_num)
-    return is_path_more_efficient(cur_num + 1, target, jolts) + \
-        is_path_more_efficient(cur_num + 2, target, jolts) + \
-        is_path_more_efficient(cur_num + 3, target, jolts)
+    out = 0
+    for incr in [1, 2, 3]:
+        next_num = cur_num + incr
+        if next_num in jolts:
+            out += is_path(next_num, target, jolts)
+    return out
 
 
-def puzzle2_more_efficient(jolts):
+def puzzle2_recursive_takes_too_long_and_does_not_work(jolts):
     start = time.time()
     max_jolts = max(jolts)
     print(max_jolts)
@@ -54,11 +36,26 @@ def puzzle2_more_efficient(jolts):
     print(end - start)
 
 
+def puzzle2(jolts):
+    max_jolts = max(jolts)
+
+    count = defaultdict(int)
+    # memoize the number of paths from each adapter to the target, and keep track of the count
+    count[jolts[0]] = 1
+    for i in range(1, len(jolts)):
+        # how many paths are there from this element to the sink?
+        jolt = jolts[i]
+        for j in [1, 2, 3]:
+            if (jolt - j) in count:
+                count[jolt] += count[jolt - j]
+    print(count[max_jolts])
+
+
 def main():
     with open('inputs/input10.txt') as f:
         jolts = sorted(list(map(int, f.read().split('\n'))))
     jolts.insert(0, 0)  # diff to original voltage
-    puzzle2_more_efficient(jolts)
+    puzzle2(jolts)
 
 
 if __name__ == "__main__":
